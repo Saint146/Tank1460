202f5a39d0ad83f9ffb6f23affb57b6169482ce5
Saint146 <saintperm@gmail.com>
2024-01-22 22:22:52 +0500

    import from binary raw version

--- Tank1460.LevelEditor/FormMain.Designer.cs
+++ Tank1460.LevelEditor/FormMain.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(12, 70);
+            button1.Name = "button1";
+            button1.Size = new Size(529, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Parse level";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Font = new Font("Cascadia Mono", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(12, 99);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(529, 425);
+            textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(62, 7);
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(72, 23);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Hexadecimal = true;
+            numericUpDown2.Location = new Point(264, 7);
+            numericUpDown2.Maximum = new decimal(new int[] { 1410065407, 2, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(88, 23);
+            numericUpDown2.TabIndex = 3;
+            numericUpDown2.Value = new decimal(new int[] { 12426, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Level #";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(172, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Level 1 address";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(358, 7);
+            button2.Name = "button2";
+            button2.Size = new Size(183, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Try to auto-detect";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(12, 41);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(529, 23);
+            textBox2.TabIndex = 7;
+            textBox2.Text = "D:\\games\\!Emulators\\NES\\Battle City (J).nes";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(553, 536);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "FormMain";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private TextBox textBox2;
     }
 }
--- Tank1460.LevelEditor/FormMain.cs
+++ Tank1460.LevelEditor/FormMain.cs
@@ -1,10 +1,138 @@
-namespace Tank1460.LevelEditor
+using System.Text;
+
+namespace Tank1460.LevelEditor;
+
+public partial class FormMain : Form
 {
-    public partial class FormMain : Form
+    public FormMain()
     {
-        public FormMain()
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        var fileName = textBox2.Text;
+
+        var level1Address = (int)numericUpDown2.Value;
+        var targetLevelNumber = (int)numericUpDown1.Value;
+
+        textBox1.Text = ParseLevelFromRom(fileName, targetLevelNumber, level1Address);
+    }
+
+    private static string ParseLevelFromRom(string fileName, int targetLevelNumber, int level1Address = 0x308A)
+    {
+        byte[] romBytes;
+        using (var binaryReader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            romBytes = binaryReader.ReadBytes((int)binaryReader.BaseStream.Length);
+
+        const int levelLengthInBytes = 0x5B;
+        var targetLevelAddress = level1Address + (targetLevelNumber - 1) * levelLengthInBytes;
+
+        var levelBytes = new byte[levelLengthInBytes];
+        Array.Copy(romBytes, targetLevelAddress, levelBytes, 0, levelLengthInBytes);
+
+        var decodedData = new char[26, 26];
+
+        // Укладываем байты в матрицу с переводом в наши символьные обозначения.
+        for (var i = 0; i < levelBytes.Length; i++)
         {
-            InitializeComponent();
+            var x = (i % 7) * 4;
+
+            var y = (i / 7) * 2;
+
+            var chars = DecodeHex(levelBytes[i] >> 4); // старшие биты
+            decodedData[x + 0, y + 0] = chars[0];
+            decodedData[x + 1, y + 0] = chars[1];
+            decodedData[x + 0, y + 1] = chars[2];
+            decodedData[x + 1, y + 1] = chars[3];
+
+            if (x == 24)
+                continue; // Каждая строка заканчивается на лишние четыре байта с 0xD.
+
+            chars = DecodeHex(levelBytes[i] & 0x0F); // младшие биты
+            decodedData[x + 2, y + 0] = chars[0];
+            decodedData[x + 3, y + 0] = chars[1];
+            decodedData[x + 2, y + 1] = chars[2];
+            decodedData[x + 3, y + 1] = chars[3];
+
         }
+
+        // Захардкоженные сущности на всех уровнях.
+        //decodedData[0, 0] = decodedData[12, 0] = decodedData[24, 0] = 'R';
+        //decodedData[8, 24] = '1';
+        //decodedData[12, 24] = 's';
+        //decodedData[16, 24] = '2';
+        //decodedData[11, 23] = decodedData[12, 23] = decodedData[13, 23] = decodedData[14, 23] =
+        //    decodedData[11, 24] = decodedData[14, 24] = decodedData[11, 25] = decodedData[14, 25] = 'X';
+
+        var sb = new StringBuilder();
+        for (var y = 0; y < decodedData.GetLength(1); y++)
+        {
+            for (var x = 0; x < decodedData.GetLength(0); x++)
+                sb.Append(decodedData[x, y] == '\0' ? '?' : decodedData[x, y]);
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    private static string DecodeHex(int p0) =>
+        p0 switch
+        {
+            0x0 => ".X.X",
+            0x1 => "..XX",
+            0x2 => "X.X.",
+            0x3 => "XX..",
+            0x4 => "XXXX",
+            0x5 => ".Q.Q",
+            0x6 => "..QQ",
+            0x7 => "Q.Q.",
+            0x8 => "QQ..",
+            0x9 => "QQQQ",
+            0xA => "~~~~",
+            0xB => "####",
+            0xC => "////",
+            _ => "...."
+        };
+
+    private static int SearchInByteArray(byte[] source, byte[] pattern)
+    {
+        var firstCharLastPos = source.Length - pattern.Length + 1;
+        for (var i = 0; i < firstCharLastPos; i++)
+        {
+            if (source[i] != pattern[0])
+                continue;
+
+            for (var j = pattern.Length - 1; j >= 1; j--)
+            {
+                if (source[i + j] != pattern[j]) break;
+                if (j == 1) return i;
+            }
+        }
+        return -1;
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        var fileName = textBox2.Text;
+        var level1Address = TryFindLevel1Address(fileName);
+
+        numericUpDown2.Value = level1Address;
+    }
+
+    private int TryFindLevel1Address(string fileName)
+    {
+        byte[] romBytes;
+        using (var binaryReader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            romBytes = binaryReader.ReadBytes((int)binaryReader.BaseStream.Length);
+
+        // В хаках начальный адрес уровней заранее неизвестен, поэтому ищем его по характерной предваряющей последовательности.
+        var prefixPattern = new byte[] { 0x18, 0x69, 0x10, 0x85, 0x57, 0xc9, 0xe0, 0xd0, 0xbd, 0x60 };
+
+        var prefixAddress = SearchInByteArray(romBytes, prefixPattern);
+        if (prefixAddress == -1)
+            return -1;
+
+        return prefixAddress + prefixPattern.Length;
     }
 }
